489689ca4e54ee17f3dfafabb149018bce91f7a6
Naki <nbsp1221@gmail.com>
2024-02-16 21:01:08 +0900

    Create database initialization code

---
+++ src/init.py
@@ -0,0 +1,41 @@
+import utils.db as db
+
+connection = db.get_connection()
+cursor = connection.cursor()
+
+# 에브리타임 게시글 테이블 생성
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS everytime_articles (
+        id VARCHAR(255) PRIMARY KEY,
+        is_notice TINYINT(1),
+        is_question TINYINT(1),
+        title TEXT,
+        text TEXT,
+        created_at DATETIME,
+        like_count INT,
+        scrap_count INT,
+        comment_count INT,
+        first_comment_id VARCHAR(255),
+        user_type VARCHAR(255),
+        user_id VARCHAR(255),
+        user_nickname VARCHAR(255),
+        user_picture_url VARCHAR(255)
+    )
+''')
+
+# 에브리타임 댓글 테이블 생성
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS everytime_comments (
+        id VARCHAR(255) PRIMARY KEY,
+        article_id VARCHAR(255),
+        parent_id VARCHAR(255),
+        text TEXT,
+        created_at DATETIME,
+        like_count INT,
+        user_type VARCHAR(255),
+        user_id VARCHAR(255),
+        user_nickname VARCHAR(255),
+        user_picture_url VARCHAR(255),
+        FOREIGN KEY (article_id) REFERENCES everytime_articles(id)
+    )
+''')
---
+++ src/utils/db.py
@@ -0,0 +1,29 @@
+import pymysql
+import utils.env as env
+from pymysql import Error
+
+connection = None
+
+def get_connection():
+    global connection
+    try:
+        if connection is None:
+            connection = pymysql.connect(
+                host=env.MYSQL_HOST,
+                port=env.MYSQL_PORT,
+                user=env.MYSQL_USER,
+                password=env.MYSQL_PASSWORD,
+                database=env.MYSQL_DATABASE,
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor
+            )
+            print('Connected to DB successfully.')
+    except Error as error:
+        print(f'Failed to connect to DB. {error}')
+    return connection
+
+def close_connection():
+    global connection
+    if connection is not None:
+        connection.close()
+        connection = None
