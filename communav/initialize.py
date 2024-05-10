dd31368d916a5fe1fea19867734acdbd7dad2dc3
Naki <nbsp1221@gmail.com>
2024-05-11 04:40:31 +0900

    Update DB initialization code

--- communav/initialize.py
+++ communav/initialize.py
@@ -49,7 +49,6 @@ cursor.execute('''
         text TEXT,
         category_id INT,
         is_verified TINYINT(1),
-        predicted_category_id INT,
         probs_category_0 FLOAT,
         probs_category_1 FLOAT,
         probs_category_2 FLOAT,
@@ -65,6 +64,31 @@ cursor.execute('''
         probs_category_12 FLOAT,
         probs_category_13 FLOAT,
         FOREIGN KEY (id) REFERENCES everytime_original_articles(id)
+    )
+''')
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS everytime_article_dataset_v1 (
+        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+        article_id BIGINT,
+        title TEXT,
+        text TEXT,
+        category_id INT,
+        is_verified TINYINT(1),
+        possible_category_id INT,
+        probs_category_0 FLOAT,
+        probs_category_1 FLOAT,
+        probs_category_2 FLOAT,
+        probs_category_3 FLOAT,
+        probs_category_4 FLOAT,
+        probs_category_5 FLOAT,
+        probs_category_6 FLOAT,
+        probs_category_7 FLOAT,
+        probs_category_8 FLOAT,
+        probs_category_9 FLOAT,
+        probs_category_10 FLOAT,
+        probs_category_11 FLOAT,
+        probs_category_12 FLOAT,
+        probs_category_13 FLOAT
     )
 ''')
 
